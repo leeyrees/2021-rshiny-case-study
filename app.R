0cc5d48da9e4b5ee84d81a38b16fd69c1abe768a
leeyrees <leyre.sastre@gmail.com>
2021-03-03 15:13:22 +0100

    filtering by year

---
+++ app.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(gapminder)
+library(tidyverse)
+
+dataPanel <- tabPanel("Data",
+                      selectInput(
+                        inputId = "selYear",
+                        label = "Select the Year",
+                        choices = gapminder %>% select(year) %>% unique %>% arrange
+                      ),
+                      tableOutput("data")
+)
+# Define UI for application that draws a histogram
+ui <- navbarPage("shiny App",
+                 dataPanel
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) { 
+  output$data <- renderTable(gapminder %>% filter(year == input$selYear))
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
